1522a1112ad5757ed0a48d587b2bb716a9c52374
KKarmakar036 <krishnendu036@gmail.com>
2024-01-01 00:37:45 +0530

    first commit

---
+++ php.js
@@ -0,0 +1,8 @@
+let list_open=false
+
+document.getElementById('toggle').addEventListener('click',function toggling
+(){
+    if(list_open==false){
+        document.getElementById('right').style.display="block";
+    }
+})
